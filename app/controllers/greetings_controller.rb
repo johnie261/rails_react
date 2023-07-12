05f5df4b2b0fba1281597c9d1f7187422a314f4f
johnie261 <njorogejohn261@gmail.com>
2023-07-12 16:58:30 +0300

    Create greetings controller

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,9 @@
+
+    class GreetingsController < ApplicationController
+        def random_greeting
+            random_greeting = Message.order('RANDOM()').first
+            render json: { greeting: random_greeting&.greeting }
+          end
+    end
+  
+  
